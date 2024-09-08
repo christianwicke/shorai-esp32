974be7a7141daad45dc6010487c4b4ac95a126c6
Christian <gitlab-fg9@c.fg9.eu>
2024-09-08 14:52:07 +0200

    adapted to MicroPython api change

--- main/ota_updater.py
+++ main/ota_updater.py
@@ -223,7 +223,7 @@ class HttpClient:
         try:
             s.connect(ai[-1])
             if proto == 'https:':
-                s = ussl.wrap_socket(s, server_hostname=host)
+                s = ssl.wrap_socket(s, server_hostname=host)
             s.write(b'%s /%s HTTP/1.0\r\n' % (method, path))
             if not 'Host' in headers:
                 s.write(b'Host: %s\r\n' % host)
