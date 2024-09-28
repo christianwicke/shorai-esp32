78e238a3d1ea8a789b8b6b1e542b8aa7a469c6ae
Christian <gitlab-fg9@c.fg9.eu>
2024-09-28 23:10:54 +0200

    switch to mrequests for downloading new version
    the old HttpClient gave ENOMEM

--- main/ota_updater.py
+++ main/ota_updater.py
@@ -1,7 +1,7 @@
-import usocket
 import os
 import gc
 import machine
+from main import mrequests
 
 header = {
     'User-Agent': 'My User Agent 1.0',
@@ -12,7 +12,6 @@ header = {
 class OTAUpdater:
 
     def __init__(self, github_repo, module='', main_dir='main'):
-        self.http_client = HttpClient()
         self.github_repo = github_repo.rstrip('/').replace('https://github.com', 'https://api.github.com/repos')
         self.main_dir = main_dir
         self.module = module.rstrip('/')
@@ -128,7 +127,8 @@ class OTAUpdater:
     
 
     def download_all_files(self, root_url, version):
-        file_list = self.http_client.get(root_url + '?ref=refs/tags/' + version)
+        file_list = mrequests.get(root_url + '?ref=refs/tags/' + version,  headers=header)
+        # {b"Accept": b"application/json", b"User-Agent": b"MicroPython OTAUpdater"}
         for file in file_list.json():
             if file['type'] == 'file':
                 download_url = file['download_url']
@@ -143,14 +143,10 @@ class OTAUpdater:
 
     def download_file(self, url, path):
         print('\tDownloading: ', path)
-        with open(path, 'w') as outfile:
-            try:
-                response = self.http_client.get(url)
-                outfile.write(response.text)
-            finally:
-                response.close()
-                outfile.close()
-                gc.collect()
+        r = mrequests.get(url)
+        r.save(path)
+        r.close()
+        gc.collect()
 
     def modulepath(self, path):
         return self.module + '/' + path if self.module else path
@@ -164,134 +160,3 @@ class OTAUpdater:
             # possibly handle other errno cases here, otherwise finally:
             else:
                 raise
-
-
-
-class Response:
-
-    def __init__(self, f):
-        self.raw = f
-        self.encoding = 'utf-8'
-        self._cached = None
-
-    def close(self):
-        if self.raw:
-            self.raw.close()
-            self.raw = None
-        self._cached = None
-
-    @property
-    def content(self):
-        if self._cached is None:
-            try:
-                self._cached = self.raw.read()
-            finally:
-                self.raw.close()
-                self.raw = None
-        return self._cached
-
-    @property
-    def text(self):
-        return str(self.content, self.encoding)
-
-    def json(self):
-        import ujson
-        return ujson.loads(self.content)
-
-
-class HttpClient:
-
-    def request(self, method, url, data=None, json=None, headers={}, stream=None):
-        try:
-            proto, dummy, host, path = url.split('/', 3)
-        except ValueError:
-            proto, dummy, host = url.split('/', 2)
-            path = ''
-        if proto == 'http:':
-            port = 80
-        elif proto == 'https:':
-            import ssl
-            port = 443
-        else:
-            raise ValueError('Unsupported protocol: ' + proto)
-
-        if ':' in host:
-            host, port = host.split(':', 1)
-            port = int(port)
-
-        ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
-        ai = ai[0]
-
-        s = usocket.socket(ai[0], ai[1], ai[2])
-        try:
-            s.connect(ai[-1])
-            if proto == 'https:':
-                s = ssl.wrap_socket(s, server_hostname=host)
-            s.write(b'%s /%s HTTP/1.0\r\n' % (method, path))
-            if not 'Host' in headers:
-                s.write(b'Host: %s\r\n' % host)
-            # Iterate over keys to avoid tuple alloc
-            for k in headers:
-                s.write(k)
-                s.write(b': ')
-                s.write(headers[k])
-                s.write(b'\r\n')
-            # add user agent
-            s.write('User-Agent')
-            s.write(b': ')
-            s.write('MicroPython OTAUpdater')
-            s.write(b'\r\n')
-            if json is not None:
-                assert data is None
-                import ujson
-                data = ujson.dumps(json)
-                s.write(b'Content-Type: application/json\r\n')
-            if data:
-                s.write(b'Content-Length: %d\r\n' % len(data))
-            s.write(b'\r\n')
-            if data:
-                s.write(data)
-
-            l = s.readline()
-            # print(l)
-            l = l.split(None, 2)
-            status = int(l[1])
-            reason = ''
-            if len(l) > 2:
-                reason = l[2].rstrip()
-            while True:
-                l = s.readline()
-                if not l or l == b'\r\n':
-                    break
-                # print(l)
-                if l.startswith(b'Transfer-Encoding:'):
-                    if b'chunked' in l:
-                        raise ValueError('Unsupported ' + l)
-                elif l.startswith(b'Location:') and not 200 <= status <= 299:
-                    raise NotImplementedError('Redirects not yet supported')
-        except OSError:
-            s.close()
-            raise
-
-        resp = Response(s)
-        resp.status_code = status
-        resp.reason = reason
-        return resp
-
-    def head(self, url, **kw):
-        return self.request('HEAD', url, **kw)
-
-    def get(self, url, **kw):
-        return self.request('GET', url, **kw)
-
-    def post(self, url, **kw):
-        return self.request('POST', url, **kw)
-
-    def put(self, url, **kw):
-        return self.request('PUT', url, **kw)
-
-    def patch(self, url, **kw):
-        return self.request('PATCH', url, **kw)
-
-    def delete(self, url, **kw):
-        return self.request('DELETE', url, **kw)
